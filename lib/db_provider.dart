f61c0785b3cec8dfb275b2ae76615c35e0d65278
kohenro19 <39029637+kohenro19@users.noreply.github.com>
2022-10-25 09:35:26 +0900

    20221025r1

--- lib/db_provider.dart
+++ lib/db_provider.dart
@@ -5,7 +5,8 @@ import 'package:sqflite/sqflite.dart' as sql;
 import 'meigen.dart';
 
 class DBProvider {
-  static Future<sql.Database> db() async {
+  static Future<sql.Database> createDB() async {
+    // initialize the database
     return sql.openDatabase(
       'meigen.db',
       version: 1,
@@ -15,13 +16,24 @@ class DBProvider {
     );
   }
 
+  // initialize the table
   static Future<void> createTables(sql.Database database) async {
     await database.execute("""CREATE TABLE meigen(
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         meigen TEXT,
         author TEXT,
+        category TEXT
      )
       """);
+    await database.execute("""
+        INSERT INTO table_name (meigen, author, category) VALUES ("happy wife", "keita", "marrige";
+      """);
+  }
+
+  static Future<List<Map<String, dynamic>>> getMeigens() async {
+    // dbをインスタンス化
+    final db = await DBProvider.createDB();
+    return db.query('meigen', orderBy: "id");
   }
 
  
