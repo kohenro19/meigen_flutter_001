6ef07bf5cbb33279f6c3468a838902fef19ee8d9
kohenro19 <39029637+kohenro19@users.noreply.github.com>
2023-03-12 11:23:26 +0900

    20230312r2

--- lib/success_page.dart
+++ lib/success_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/favorites.dart';
 
 class success_page extends StatelessWidget {
   success_page({Key? key}) : super(key: key);
@@ -32,24 +34,62 @@ Map meigens = {
         )
       ),
  
-      body: Center(
+ body: Center(
         child: ListView.builder(
           itemCount: meigens.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              margin: EdgeInsets.only(
-                bottom: 20  // 行間を広げる
-              ),
-              child: ListTile(
-              // selected: true,
-              title: Text(meigens[index]['meigen']), // 名言
-              subtitle: Text(meigens[index]["person"]), // 人物
-          ),
-        );
-      },
-        ),
+          itemBuilder: (BuildContext context, index)  => ItemTile(index, meigens[index]['meigen']),
+ 
       )
       
+    )
+    );
+  }
+}
+
+class ItemTile extends StatelessWidget {
+  final int itemNo;
+  final String meigen;
+
+  const ItemTile(
+    this.itemNo,
+    this.meigen
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    // Provider.of<Favorites>(context) で親Widgetからデータを受け取る
+    var favoritesList = Provider.of<Favorites>(context);
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Colors.primaries[itemNo % Colors.primaries.length],
+        ),
+        title: Text(
+          'Item $itemNo $meigen',
+          key: Key('text_$itemNo'),
+        ),
+        trailing: IconButton(
+          key: Key('icon_$itemNo'),
+          icon: favoritesList.items.contains(itemNo)
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          onPressed: () {
+            !favoritesList.items.contains(itemNo)
+                ? favoritesList.add(itemNo)
+                : favoritesList.remove(itemNo);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(favoritesList.items.contains(itemNo)
+                    ? 'Added to favorites.'
+                    : 'Removed from favorites.'),
+                duration: Duration(seconds: 1),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
